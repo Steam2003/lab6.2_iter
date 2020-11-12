40e5cc8a301f6494fc8327c196095b03e5a3f967
Ivan Patiuk <gendalf2003@gmail.com>
2020-11-12 13:38:36 +0200

    lab6.2_iter_fix

--- lab6.2_iter.cpp
+++ lab6.2_iter.cpp
@@ -3,41 +3,12 @@
 #include <iomanip>
 
 using namespace std;
-void iter(int* mas, int Low, int Max, int N)
-{
-	for (int i = 0; i <= N - 1; ++i)
-	{
-		mas[i] = Low + rand() % (Max - Low + 1);
-		cout << setw(3) << mas[i] << " ";
-	}
-	int min = mas[0];
-	int max = mas[0];
-	for (int i = 0; i <= N-1; ++i)
-	{
-		if (mas[i] < min)
-			min = mas[i];
-		if (mas[i] > max)
-			max = mas[i];
-	}
-	cout << endl;
-	cout << "Сума максимального та мінімального числа масиву: " << min + max << endl;
-}
-int unittest(int c[])
-{
-	int i = 0;
-	int N = 10;
-	int min = c[i];
-	int max = c[i];
-	for (i = 0; i <= N - 1; ++i)
-	{
-		if (c[i] < min)
-			min = c[i];
-		if (c[i] > max)
-			max = c[i];
-	}
-	cout << "Unit test result: " << min + max << endl;
-	return min + max;
-}
+
+void create(int* mas, int Low, int Max, int N);
+void print(int* mas, int N);
+int MinE(int* mas, int N);
+int MaxE(int* mas, int N);
+
 int main()
 {
 	int N, Low, Max;
@@ -50,8 +21,40 @@ int main()
 	cout << endl;
 	int* mas = new int[N];
 	cout << "Ітераційний" << endl;
-	iter(mas, Low, Max, N);
+	create(mas, Low, Max, N);
+	print(mas, N);
+	cout << endl;
+	int min= MinE(mas, N);
+	int max =MaxE(mas, N);
+	cout << "Мінімальне: " << min << endl;
+	cout << "Максимальне: " << max << endl;
+	cout << "Сума максимального та мінімального: " << min + max << endl;
 	delete[] mas;
-	int c[10] = { -10,5,9,2,7,-23,33,4,17,-15 };
-	unittest(c);
+	cin.get();
+}
+void create(int* mas, int Low, int Max, int N)
+{
+	for (int i = 0; i <= N - 1; ++i)
+		mas[i] = Low + rand() % (Max - Low + 1);
+}
+void print(int* mas, int N)
+{
+	for (int i = 0; i <= N - 1; ++i)
+		cout << setw(3) << mas[i] << " ";
+}
+int MinE(int* mas, int N)
+{
+	int min = mas[0];
+	for (int i = 0; i <= N - 1; ++i)
+		if (mas[i] < min)
+			min = mas[i];
+	return min;
+}
+int MaxE(int* mas, int N)
+{
+	int max = mas[0];
+	for (int i = 0; i <= N - 1; ++i)
+		if (mas[i] > max)
+			max = mas[i];
+	return max;
 }
